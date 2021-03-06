5e17d23cd049baa701a77eecd71797f631c14dcb
chiaaifen <chiaaifen@gmail.com>
2021-03-06 17:47:26 +0800

    New codes for NLP (Sentiment Analysis)

--- get_scores.py
+++ get_scores.py
@@ -1,20 +1,108 @@
-# import nltk
-# nltk.download('vader_lexicon')
+import re, string, unicodedata
+import nltk
+# import contractions
+import inflect
+from bs4 import BeautifulSoup
+from nltk import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+from nltk.stem import LancasterStemmer, WordNetLemmatizer
 
 from nltk.sentiment import SentimentIntensityAnalyzer
+
 import pandas as pd
 
+def remove_non_ascii(words):
+    """Remove non-ASCII characters from list of tokenized words"""
+    new_words = []
+    for word in words:
+        new_word = unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+        new_words.append(new_word)
+    return new_words
+
+def to_lowercase(words):
+    """Convert all characters to lowercase from list of tokenized words"""
+    new_words = []
+    for word in words:
+        new_word = word.lower()
+        new_words.append(new_word)
+    return new_words
+
+def remove_punctuation(words):
+    """Remove punctuation from list of tokenized words"""
+    new_words = []
+    for word in words:
+        new_word = re.sub(r'[^\w\s]', '', word)
+        if new_word != '':
+            new_words.append(new_word)
+    return new_words
+
+def replace_numbers(words):
+    """Replace all interger occurrences in list of tokenized words with textual representation"""
+    p = inflect.engine()
+    new_words = []
+    for word in words:
+        if word.isdigit():
+            new_word = p.number_to_words(word)
+            new_words.append(new_word)
+        else:
+            new_words.append(word)
+    return new_words
+
+def remove_stopwords(words):
+    """Remove stop words from list of tokenized words"""
+    new_words = []
+    for word in words:
+        if word not in stopwords.words('english'):
+            new_words.append(word)
+    return new_words
+
+def stem_words(words):
+    """Stem words in list of tokenized words"""
+    stemmer = LancasterStemmer()
+    stems = []
+    for word in words:
+        stem = stemmer.stem(word)
+        stems.append(stem)
+    return stems
+
+def lemmatize_verbs(words):
+    """Lemmatize verbs in list of tokenized words"""
+    lemmatizer = WordNetLemmatizer()
+    lemmas = []
+    for word in words:
+        lemma = lemmatizer.lemmatize(word, pos='v')
+        lemmas.append(lemma)
+    return lemmas
+
+def normalize(words):
+    words = remove_non_ascii(words)
+    words = to_lowercase(words)
+    words = remove_punctuation(words)
+    words = replace_numbers(words)
+    words = remove_stopwords(words)
+    return words
+
 def get_scores(data):
-    """Returns Sentiment Analysis Scores"""
     
+    lst_scores = []
+    
+    """Returns Sentiment Analysis Scores"""
+   
     sia = SentimentIntensityAnalyzer()
 
     for index, row in data.iterrows():
-        score = sia.polarity_scores(row['title'])
-        print(score['compound'])
+        title = row['title']
+        text_tokens = word_tokenize(title)
+        cleaned_tokens = normalize(text_tokens)
+        lemmas = lemmatize_verbs(cleaned_tokens)
+        cleaned_sentence = (" ").join(cleaned_tokens) # filtered sentences 
 
-
-if __name__ == "__main__":
-    data = pd.read_csv("test.csv", encoding='unicode_escape')
+        score = sia.polarity_scores(cleaned_sentence)
+        lst_scores.append(score)
+        # print(score['compound'])
     
-    get_scores(data)
+    data['scores'] = lst_scores
+        
+        
+        
