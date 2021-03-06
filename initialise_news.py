336f566f9b41816b279c5b36413bf771826c8951
gooohjy <gooohjiayi@gmail.com>
2021-03-06 16:59:11 +0800

    Removal of API pool and update

--- initialise_news.py
+++ initialise_news.py
@@ -5,11 +5,20 @@ import pandas as pd
 import time
 import nltk
 import string
+import os
 from dateutil import parser
 from datetime import date, datetime, timedelta
-from apipool import ApiKey, ApiKeyManager
 
 import config
+
+def print_banner(text, length=70):
+    print(">"*length)
+    print(text)
+    print("<"*length)
+
+def print_line(text, length=70):
+    side_length = max(int((50-len(text))/2), 0)
+    print("="*side_length + text + "="*side_length)
 
 def preprocess_text(text):
     """Removing source and punctuations from title"""
@@ -25,95 +34,102 @@ def extract_news(company):
 
     # Initialise api request variables
     url = "https://google-news.p.rapidapi.com/v1/search"
-    headers = {
-    'x-rapidapi-host': "google-news.p.rapidapi.com",
-    'x-rapidapi-key': api_key
-    }
+    api_key_index = 0 # Start with first key
 
     # Initialise variables
-    extract_start_date = start_date
+    extract_start_date = start_date # Start with first day
 
     while extract_start_date < end_date:
-        extract_end_date = extract_start_date + timedelta(days=7) # End of week
+        # Initialise api request key (for rotation)
+        headers = {
+            'x-rapidapi-host': "google-news.p.rapidapi.com",
+            'x-rapidapi-key': api_keys[api_key_index]
+            }
+
+        # To query in the same ISO week only
+        if extract_start_date.weekday() == 6: # 6 refers to Sunday index
+            extract_end_date = extract_start_date + timedelta(days=7) # End of week
+        else:
+            days_to_sunday = timedelta((6 - extract_start_date.weekday()) % 7)
+            extract_end_date = extract_start_date + days_to_sunday # End of week
+        
+        # Ensure extract end date does not exceed end date
+        if extract_end_date > end_date:
+            extract_end_date = end_date
 
         # Make api call
-        query_string = {"country":"US", "lang":"en", "q":company, "from":str(extract_start_date), "to":str(extract_end_date)}
-        response = requests.request("GET", url, headers=headers, params=query_string)
+        query_string = {"country":"US", "lang":"en", "q":company, 
+            "from":str(extract_start_date), "to":str(extract_end_date)
+            }
+        response = requests.request("GET", url, headers=headers, 
+            params=query_string)
         response_text = json.loads(response.text)
 
         # Check if api limit has been reached
         if 'message' in response_text:
-            print("API limit reached, sleeping for 1 hour")
-            time.sleep(3600)
+            # Update api key index
+            if api_key_index + 1 < len(api_keys):
+                new_api_key_index = api_key_index + 1
+                print_line("API limit reached for API key index {}, switching to {}".format(api_key_index, new_api_key_index))
+            else:
+                new_api_key_index = 0
+                print_line("API limit reached for all API keys, sleeping for 1 hour")
+                time.sleep(3600)
+            
+            # Update API key
+            api_key_index = new_api_key_index
             continue
 
         print("Progress: Extracted {} for the time period {} to {}".format(company, extract_start_date, extract_end_date))
+
         # Save query results progressively
         with open(output_path, 'a', newline='') as f:
             writer = csv.writer(f)
 
             for article in response_text['articles']:
-                title = preprocess_text(article["title"]).encode('utf-8')
+                title = preprocess_text(article["title"]) #.encode('utf-8')
                 date = datetime.strptime(article["published"], '%a, %d %b %Y %H:%M:%S %Z')
                 date_posted = date.strftime('%Y-%m-%d')
                 year = date.isocalendar()[0] # Extracting year
                 week = date.isocalendar()[1] # Extracting week
                 writer.writerow([company, title, date_posted, year, week])
 
-        extract_start_date = extract_end_date # Next week data
-
-# class RapidApiKey(ApiKey):
-#     def __init__(self,
-#                  api_key)
-#         self.api_key = api_key
-
-#     def get_primary_key(self):
-#         return self.api_key
-
-#     def test_usable(self, api_key):
-#         query_string = {"country":"US", "lang":"en", "q":company, "from":str(extract_start_date), "to":str(extract_end_date)}
-#         response = requests.request("GET", url, headers=headers, params=query_string)
-#         response_text = json.loads(response.text)
-
-#         # Check if api limit has been reached
-#         if 'message' in response_text:
-#             return False
-#         else:
-#             return True
-
-# USE https://pypi.org/project/apipool/#description
+        # Update extract start date to obtain next week data
+        extract_start_date = extract_end_date
+        
 
 if __name__ == "__main__":
-    # Set parameters (year, month, day)
-    start_date = date(2020, 2, 12)
-    end_date = date(2020, 6, 30)
+    # Set date parameters (year, month, day)
+    start_date = date(config.START_DATE[0], config.START_DATE[1], config.START_DATE[2])
+    end_date = date(config.END_DATE[0], config.END_DATE[1], config.END_DATE[2])
     print("Start date: " + start_date.strftime('%Y-%m-%d'))
     print("End date: " + end_date.strftime('%Y-%m-%d'))
     today = date.today()
 
-    # TODO: Edit the program to allow multiple API keys and concurrent retrieval
-    # api_keys = config.API_KEYS
-    # api_keys_list = [
-    #     RapidApiKey(**api_key)
-    #     for api_key in api_keys
-    # ]
-    # manager = ApiKeyManager(apikey_list=api_keys_list)
+    # Obtain API keys
+    api_keys = config.API_KEYS
+    print("{} API keys received".format(len(api_keys)))
 
-    api_key = config.API_KEYS[0]
-
+    # Set path
     companies_path = 'companies.txt'
-    output_path = 'initialise_news {}.csv'.format(today)
+    output_path = 'data/initialise_news {}.csv'.format(today)
 
     # Write header for output file
     col_names = ['company_name', 'title', 'date_posted', 'year', 'week'] # 'score'
-    with open(output_path, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(col_names)
+    
+    # Create new file with headers if file does not exist
+    if not os.path.isfile(output_path):
+        print("Creating new file for storage at {}".format(output_path))
+        with open(output_path, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(col_names)    
+    else:
+        print("Storing in existing file {}".format(output_path))
 
     # Load companies for extraction
     companies = open(companies_path).read().split()
     
     # Extract news for all companies
     for company in companies:
-        print("Extracting news: " + company)
+        print_banner("Extracting news: " + company)
         extract_news(company)
