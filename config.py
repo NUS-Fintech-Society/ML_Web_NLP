71a70f03e237864126b1c8414766b6160c8ef4fa
gooohjy <gooohjiayi@gmail.com>
2021-03-06 17:03:25 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+# [year, month, day]
+START_DATE = [2020, 1, 1]
+END_DATE = [2020, 6, 30]
+
 API_KEYS = [
-    "265674b26emshe10b26d3bbe9aedp145b87jsn132c610f8ab3",
-    "2c7afae35amsh3cc1f097bc8024ap1560ecjsn45e29a3b78da",
-    "1b488d633emsh0097895a7df0eebp1b4b91jsnb239c08c927e",
+    
 ]
